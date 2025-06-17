96bd6c91e1c6ee9a4b6f3839f7f41ebfb4106cf3
Zarya-MD <dawenstech@gmail.com>
2025-06-17 04:27:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -728,7 +728,7 @@ if (!isReact && config.CUSTOM_REACT === 'true') {
                         global.email
                     }\nitem2.X-ABLabel:GitHub\nitem3.URL:https://github.com/${
                         global.github
-                    }/gotar-xmd\nitem3.X-ABLabel:GitHub\nitem4.ADR:;;${
+                    }/izuka-md\nitem3.X-ABLabel:GitHub\nitem4.ADR:;;${
                         global.location
                     };;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
                 });
