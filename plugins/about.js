595fc7d4127f13a108b4d66a4a0995a6bc3bf7d3
Zarya-MD <dawenstech@gmail.com>
2025-06-16 20:49:50 -0400

    Create about.js

---
+++ plugins/about.js
@@ -0,0 +1,56 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    alias: "dev",
+    react: "👑",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `
+*╭┈───────────────•*
+*ʜᴇʟʟᴏ 👋 ${pushname}*
+*╰┈───────────────•*
+*╭┈───────────────•*
+*│  ◦* *ᴄʀᴇᴀᴛᴇᴅ ʙʏ: dawens boy*
+*│  ◦* *ʀᴇᴀʟ ɴᴀᴍᴇ➩ Kibutsuji Muzan*
+*│  ◦* *ɴɪᴄᴋɴᴀᴍᴇ➩ dawens*
+*│  ◦* *ᴀɢᴇ➩ ɴᴏᴛ ᴅᴇғɪɴᴇᴅ*
+*│  ◦* *ᴄɪᴛʏ➩ ɴᴏᴛ ᴅᴇғɪɴᴇᴅ* 
+*│  ◦* *ᴀ ᴘᴀꜱꜱɪᴏɴᴀᴛᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ᴅᴇᴠ*
+*╰┈───────────────•*
+
+*⪨ • IZUKA-MD - ᴘʀᴏᴊᴇᴄᴛ • ⪩*
+
+*╭┈───────────────•*
+*│  ◦* *✰➩DAWENS BOY x INCONNU BOY*
+*│  ◦* *✰➩ᴏɴʟʏ 2 ᴅᴇᴠᴇʟᴏᴘᴇʀ*
+*╰┈───────────────•*
+
+*•────────────•✱*
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ DAWENS BOY*
+*•────────────•✱*
+`
+await conn.sendMessage(from, {
+    image: { url: 'https://files.catbox.moe/nsk6ix.png' },
+    caption: about,
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363398101781980@newsletter', // ou ton JID actuel
+            newsletterName: 'IZUKA-MD',
+            serverMessageId: 143
+        }
+    }
+}, { quoted: mek })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
